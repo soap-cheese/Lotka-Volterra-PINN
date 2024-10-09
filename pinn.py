09467863d35756dcc65afd13b7c1cac6f5a55d5e
soap-cheese <125205163+soap-cheese@users.noreply.github.com>
2024-10-09 19:47:27 +0300

    Add files via upload

--- pinn.py
+++ pinn.py
@@ -5,6 +5,11 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
+# class Sin(nn.Module):
+#     def forward(self, x):
+#         return torch.sin(x)
+
+
 class Net(nn.Module):
     def __init__(self, hidden_size=16):
         super().__init__()
@@ -21,7 +26,7 @@ class Net(nn.Module):
         return self.layers_stack(x)
 
 
-def pde(outt, tt):
+def pde(outt, tt, alpha, beta, gamma, delta):
     x = outt[:, 0].unsqueeze(1)
     y = outt[:, 1].unsqueeze(1)
     dxdt = torch.autograd.grad(x, tt, torch.ones_like(x), create_graph=True,
@@ -36,31 +41,50 @@ def pde(outt, tt):
     # return pde1, pde2
 
 
-def pdeloss(tt, lmbd=1):
+def pdeloss(tt, epoch, x0, y0, param, lmbd, update_array=True):
     outt = the_net(tt).to(device)
-    f = pde(outt, tt)
+    f = pde(outt, tt, *param)
 
     loss_pde = mse(f, torch.zeros_like(f))
     loss_bc = mse(outt[0], torch.Tensor([x0, y0]))
+    loss_total = lmbd * loss_bc + loss_pde
 
-    return lmbd * loss_bc + loss_pde
+    if update_array:
+        loss_pde_arr[epoch] = loss_pde
+        loss_bc_arr[epoch] = loss_bc
+        loss_arr[epoch] = loss_total
+
+    return loss_total
 
 
-def train(ep):
+def train(ep, parameters, lmbd):
     pbar = tqdm(range(ep), desc='Training Progress')
     for i in pbar:
         optimizer.zero_grad()
-        loss = pdeloss(t)
-        loss_arr[i] = loss
+        loss = pdeloss(t, i, parameters[4], parameters[5], parameters[:4], lmbd)
         loss.backward()
         optimizer.step()
 
 
-def plot_loss(loss):
-    plt.title('Loss decreasing')
-    plt.xlabel('epochs')
-    plt.ylabel('loss')
-    plt.plot(loss)
+def plot_loss(loss, lossbc, losspde, lmbd):
+    plt.subplot(3, 1, 1)
+    plt.title('Loss total')
+    plt.plot(loss, label='loss total')
+    # plt.plot(lossbc, label='loss bc')
+    # plt.plot(losspde, label='loss pde')
+    plt.legend()
+
+    plt.subplot(3, 1, 2)
+    plt.plot(lossbc*lmbd, label='loss bc * lambda')
+    plt.plot(losspde, label='loss pde')
+    plt.legend()
+
+    plt.subplot(3, 1, 3)
+    plt.plot(lossbc, label='loss bc')
+    plt.plot(losspde, label='loss pde')
+    plt.legend()
+
+    plt.subplots_adjust(hspace=0.5, wspace=0.5)
     plt.show()
 
 
@@ -81,39 +105,44 @@ def load_parameters(file='lotka-volterra-parameters.txt'):
     return res
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if __name__ == '__main__':
 
-prmtrs = load_parameters()
-t_start = int(prmtrs[0])
-t_end = int(prmtrs[1])
-t_steps = int(prmtrs[2])
-alpha = prmtrs[3]
-beta = prmtrs[4]
-gamma = prmtrs[5]
-delta = prmtrs[6]
-x0 = prmtrs[7]
-y0 = prmtrs[8]
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-t = (torch.linspace(t_start, t_end, t_steps).unsqueeze(1)).to(device)
-t.requires_grad = True
+    prmtrs = load_parameters()
+    t_start = int(prmtrs[0])
+    t_end = int(prmtrs[1])
+    t_steps = int(prmtrs[2])
 
-the_net = Net()
+    t = (torch.linspace(t_start, t_end, t_steps).unsqueeze(1)).to(device)
+    t.requires_grad = True
 
-train_switch = 0
-if train_switch:
-    mse = nn.MSELoss()
-    lr = 0.01
-    optimizer = torch.optim.Adam(the_net.parameters(), lr=lr)
+    the_net = Net()
 
-    epochs = 2000
-    loss_arr = np.zeros(epochs)
+    train_switch = 1
+    if train_switch:
+        mse = nn.MSELoss()
+        lr = 0.01
+        optimizer = torch.optim.Adam(the_net.parameters(), lr=lr)
 
-    train(epochs)
-    torch.save(the_net.state_dict(), 'Lotka–Volterra-weights.pth')
+        epochs = 1000
+        loss_arr = np.zeros(epochs)
+        loss_pde_arr = np.zeros(epochs)
+        loss_bc_arr = np.zeros(epochs)
 
-    print(loss_arr[-1])
-    plot_loss(loss_arr)
-else:
-    the_net.load_state_dict(torch.load('Lotka–Volterra-weights.pth', weights_only=True))
-    out = the_net(t)
-    plot_solution(t, out)
+        lambd = 1/10 ** 3
+
+        train(epochs, parameters=prmtrs[3:], lmbd=lambd)
+        torch.save(the_net.state_dict(), 'Lotka–Volterra-weights.pth')
+
+        print(f'average bc loss: {loss_bc_arr.mean()}')
+        print(f'average pde loss: {loss_pde_arr.mean()}')
+
+        plot_loss(loss_arr, loss_bc_arr, loss_pde_arr, lambd)
+
+        out = the_net(t)
+        plot_solution(t, out)
+    else:
+        the_net.load_state_dict(torch.load('Lotka–Volterra-weights.pth', weights_only=True))
+        out = the_net(t)
+        plot_solution(t, out)
